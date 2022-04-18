497e83b3e92e0a92e9adb733e655dfcd81456321
NB <nikodemb@student.agh.edu.pl>
2022-04-18 20:24:56 +0200

    Added a bit impaired language choice buttons

--- app/src/main/java/com/example/sanguagelogin/SignUpActivity.java
+++ app/src/main/java/com/example/sanguagelogin/SignUpActivity.java
@@ -2,8 +2,10 @@ package com.example.sanguagelogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 

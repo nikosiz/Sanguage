dd1edf60cf5918f5964e241e48ad6d97731c95a0
michal <mpuczynski55@gmail.com>
2022-04-21 16:11:51 +0000

    password validation, login logic, edit text ids

--- app/src/main/java/com/example/sanguagelogin/SignInActivity.java
+++ app/src/main/java/com/example/sanguagelogin/SignInActivity.java
@@ -5,12 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.balysv.materialripple.MaterialRippleLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class SignInActivity extends AppCompatActivity {
 
-    MaterialRippleLayout log_in_mrl;
+    private MaterialRippleLayout log_in_mrl;
+    private EditText username_email_et;
+    private EditText password_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +32,46 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         log_in_mrl = findViewById(R.id.log_in_mrl);
+        username_email_et = findViewById(R.id.username_email_et);
+        password_et = findViewById(R.id.password_et);
 
         log_in_mrl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainAppWindow.class);
-                startActivity(intent);
-            }
+                String URL = "https://sanguage.herokuapp.com/login";
+                String username_email = username_email_et.getText().toString();
+                String password = password_et.getText().toString();
+                if (username_email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "please provide correctly data", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                try {
+                    JSONObject loginJSON = createLoginJSON(username_email, password);
+                    RequestQueue queue = Volley.newRequestQueue(SignInActivity.this);
+                    JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, URL, loginJSON, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Intent intent = new Intent(getApplicationContext(), MainAppWindow.class);
+                            startActivity(intent);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    queue.add(jsonObjectRequest);
+                } catch (JSONException jsonException) {
+                    Toast.makeText(getApplicationContext(), "please provide correctly data", Toast.LENGTH_SHORT).show();
+                }
+            }}
         });
+    }
+
+    public JSONObject createLoginJSON(String username_email, String password) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("usernameOrEmail", username_email);
+        jsonObject.put("password", password);
+        return jsonObject;
     }
 }
--- app/src/main/java/com/example/sanguagelogin/SignUpActivity.java
+++ app/src/main/java/com/example/sanguagelogin/SignUpActivity.java
@@ -8,11 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+    public boolean validatePassword(String password){
+        Pattern p = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
+        Matcher m = p.matcher(password);
+        return m.matches();
     }
 }

2bff6df391274ab70f12f1ce82acebadce50dee3
mpuczynski55 <mpuczynski55@github.com>
2022-04-22 12:13:43 +0000

    signup logic

--- app/src/main/java/com/example/sanguagelogin/SignUpActivity.java
+++ app/src/main/java/com/example/sanguagelogin/SignUpActivity.java
@@ -1,15 +1,25 @@
 package com.example.sanguagelogin;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.balysv.materialripple.MaterialRippleLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,6 +28,10 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText username_et;
     private EditText email_et;
     private EditText password_et;
+    private MaterialRippleLayout signup_mrl;
+    private TextView username_tv;
+    private TextView email_tv;
+    private TextView password_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,10 @@ public class SignUpActivity extends AppCompatActivity {
         username_et = findViewById(R.id.editTextUsername);
         email_et = findViewById(R.id.editTextEmailAddress);
         password_et = findViewById(R.id.editTextPassword);
+        signup_mrl = findViewById(R.id.sign_up_mrl);
+        username_tv = findViewById(R.id.TextViewUsername);
+        email_tv = findViewById(R.id.TextViewEmailAddress);
+        password_tv = findViewById(R.id.TextViewPassword);
 
         password_et.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -49,6 +67,71 @@ public class SignUpActivity extends AppCompatActivity {
                 }
             }
         });
+        signup_mrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = username_et.getText().toString();
+                String email = email_et.getText().toString();
+                String password = password_et.getText().toString();
+                //  TODO language String
+                String secondLanguage = "";
+                boolean validateAllData = validateAllData(username, email, password);
+                if (validateAllData) {
+                    signUpRequest(username, email, password, secondLanguage);
+                }
+            }
+        });
+    }
+
+    public void signUpRequest(String username, String email, String password, String secondLanguage) {
+        String URL = "https://sanguage.herokuapp.com/registration";
+        try {
+            JSONObject signUpJSON = createSignUpJSON(username, email, password, secondLanguage);
+            RequestQueue queue = Volley.newRequestQueue(SignUpActivity.this);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL, signUpJSON, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    // next window
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    try {
+                        String message = RequestErrorParser.parseError(error);
+                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                    } catch (JSONException j) {
+                        Toast.makeText(getApplicationContext(), "unidentified error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            );
+            queue.add(jsonObjectRequest);
+        } catch (JSONException j) {
+            ;
+        }
+    }
+
+    public JSONObject createSignUpJSON(String username, String email, String password, String secondLanguage) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", username);
+        jsonObject.put("email", email);
+        jsonObject.put("password", password);
+        jsonObject.put("secondLanguage", secondLanguage);
+        return jsonObject;
+
+    }
+
+    public boolean validateAllData(String username, String email, String password) {
+        if (username.isEmpty()) {
+            username_tv.startAnimation(shakeError());
+        }
+        if (email.isEmpty() || !validateEmail(email)) {
+            email_tv.startAnimation(shakeError());
+        }
+        if (password.isEmpty() || !validatePassword(password)) {
+            password_tv.startAnimation(shakeError());
+        }
+        return !username.isEmpty() && validateEmail(email) && validatePassword(password);
     }
 
     public boolean validatePassword(String password) {
@@ -61,5 +144,12 @@ public class SignUpActivity extends AppCompatActivity {
         Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
         Matcher m = p.matcher(email);
         return m.matches();
+    }
+
+    public TranslateAnimation shakeError() {
+        TranslateAnimation shake = new TranslateAnimation(5, 15, 0, 0);
+        shake.setDuration(500);
+        shake.setInterpolator(new CycleInterpolator(7));
+        return shake;
     }
 }

a853881374d10f0e39cbf49313288ebde2d7932f
NB <nikodemb@student.agh.edu.pl>
2022-05-04 15:26:46 +0200

    Added popup filter box

--- app/src/main/java/com/example/sanguage/LearnFragment.java
+++ app/src/main/java/com/example/sanguage/LearnFragment.java
@@ -1,10 +1,19 @@
 package com.example.sanguage;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -34,6 +43,13 @@ public class LearnFragment extends Fragment {
     private ArrayList<DictionaryPojo> dictionaryListSimple = new ArrayList<>();
     private Context context;
     private String currentURL;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private TextView filter_known_words_tv, filter_new_words_tv, filter_level_tv, filter_topic_tv;
+    private CheckBox filter_known_words_cb, filter_new_words_cb, filter_level_A1_cb, filter_level_A2_cb, filter_level_B1_cb, filter_level_B2_cb, filter_level_C1_cb, filter_level_C2_cb, filter_topic_cb;
+    private Button filter_apply_btn, filter_cancel_btn;
+    ImageView filter_btn;
 
     public LearnFragment() {
     }
@@ -137,11 +153,62 @@ public class LearnFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_learn, container, false);
 
+        filter_btn = (ImageView) view.findViewById(R.id.learn_filter_btn);
+
+        filter_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                filterDialog();
+            }
+        });
+
         flingContainer = (SwipeFlingAdapterView) view.findViewById(R.id.frame_flashcard);
         currentURL = "https://sanguage.herokuapp.com/dictionary/byLanguage?language=English";
         flashcardAdapter = new FlashcardAdapter(context, R.layout.flashcard, dictionaryListSimple);
         flingContainer.setAdapter(flashcardAdapter);
         setFlingContainer();
         return view;
+    }
+
+    public void filterDialog() {
+        dialogBuilder = new AlertDialog.Builder(getContext());
+        final View filterPopupView = getLayoutInflater().inflate(R.layout.popup, null);
+
+        filter_known_words_tv = (TextView) filterPopupView.findViewById(R.id.filter_known_words_tv);
+        filter_new_words_tv = (TextView) filterPopupView.findViewById(R.id.filter_new_words_tv);
+        filter_level_tv = (TextView) filterPopupView.findViewById(R.id.filter_level_tv);
+        filter_topic_tv = (TextView) filterPopupView.findViewById(R.id.filter_topic_tv);
+
+        filter_known_words_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_known_words_cb);
+        filter_new_words_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_new_words_cb);
+        filter_level_A1_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_level_A1_cb);
+        filter_level_A2_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_level_A2_cb);
+        filter_level_B1_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_level_B1_cb);
+        filter_level_B2_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_level_B2_cb);
+        filter_level_C1_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_level_C1_cb);
+        filter_level_C2_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_level_C2_cb);
+        filter_topic_cb = (CheckBox) filterPopupView.findViewById(R.id.filter_topic_cb);
+
+        filter_apply_btn = (Button) filterPopupView.findViewById(R.id.filter_apply_btn);
+        filter_cancel_btn = (Button) filterPopupView.findViewById(R.id.filter_cancel_btn);
+
+        dialogBuilder.setView(filterPopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        filter_apply_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //apply filter action
+            }
+        });
+
+        filter_cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //apply cancel action
+                dialog.dismiss();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sanguage/ProfileFragment.java
+++ app/src/main/java/com/example/sanguage/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.sanguage;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER

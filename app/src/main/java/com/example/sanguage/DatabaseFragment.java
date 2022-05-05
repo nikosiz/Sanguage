d12ec59366a5e25433ef9f6c143e9433bbe9b7f6
mpuczynski55 <mpuczynski55@github.com>
2022-05-05 21:07:53 +0200

    database listview

--- app/src/main/java/com/example/sanguage/DatabaseFragment.java
+++ app/src/main/java/com/example/sanguage/DatabaseFragment.java
@@ -1,27 +1,50 @@
 package com.example.sanguage;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DatabaseFragment extends Fragment {
 
     private ListView database_lv;
+    private Long userID;
+    private Context context;
+    private ArrayList<String> userKnownVocab = new ArrayList<>();
+    private ArrayAdapter arrayAdapter;
 
     public DatabaseFragment() {
     }
 
-    public static DatabaseFragment newInstance(String param1, String param2) {
-        DatabaseFragment fragment = new DatabaseFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
+    public DatabaseFragment(Long userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        this.context = context;
+        super.onAttach(context);
     }
 
     @Override
@@ -30,9 +53,41 @@ public class DatabaseFragment extends Fragment {
 
     }
 
+    public void setKnownVocabularyListView() {
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        String URL = "https://sanguage.herokuapp.com/user/getKnownVocab?userID=" + userID;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                setUserKnownVocab(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Check your internet connection", Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public void setUserKnownVocab(JSONObject response) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String[] dictionary = mapper.readValue(response.toString(), String[].class);
+            userKnownVocab.addAll(Arrays.asList(dictionary));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_database, container, false);
+        View view = inflater.inflate(R.layout.fragment_database, container, false);
+        database_lv = view.findViewById(R.id.database_lv);
+        setKnownVocabularyListView();
+        arrayAdapter = new ArrayAdapter(this, R.layout.database_list_view_row, userKnownVocab);
+        return view;
+
     }
 }

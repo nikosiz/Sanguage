d08e02eecb64d8c28202c3d2f5fd4f2ee2d16968
NB <nikodemb@student.agh.edu.pl>
2022-05-05 22:31:12 +0200

    SSDD

--- app/src/main/java/com/example/sanguage/DatabaseFragment.java
+++ app/src/main/java/com/example/sanguage/DatabaseFragment.java
@@ -2,14 +2,18 @@ package com.example.sanguage;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.android.volley.Request;
@@ -85,17 +89,49 @@ public class DatabaseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_database, container, false);
+
+        //TextView asdf = (TextView) view.findViewById(R.id.database_lv);
+
+        registerForContextMenu(database_lv);
+
+        return view;
         database_lv = view.findViewById(R.id.database_lv);
         //setKnownVocabularyListView();
         userKnownVocab.add("Michalek");
         userKnownVocab.add("Lukaszek");
         userKnownVocab.add("chuj");
         userKnownVocab.add("pati");
-        arrayAdapter = new ListViewAdapter(context, R.layout.listview_row, userKnownVocab);
+        arrayAdapter = new ListViewAdapter(context, R.layout.database_listview_row, userKnownVocab);
         database_lv.setAdapter(arrayAdapter);
     // arrayAdapter.getFilter().convertResultToString("dasd");
         arrayAdapter.notifyDataSetChanged();
+
+        registerForContextMenu(database_lv);
+
         return view;
 
+    }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getActivity().getMenuInflater().inflate(R.menu.context_database, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_database_show_more:
+                Toast.makeText(getContext(), "show more", Toast.LENGTH_SHORT).show();
+                //show selected vocabulary on a flashcard
+                return true;
+            case R.id.context_database_delete:
+                Toast.makeText(getContext(), "delete", Toast.LENGTH_SHORT).show();
+                //delete selected vocabulary from database
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }

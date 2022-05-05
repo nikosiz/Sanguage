ef8d58aef99b4b6b7da8f53ec9b16fc155f3a8f3
NB <nikodemb@student.agh.edu.pl>
2022-05-05 21:57:52 +0200

    Why can't I commit this commit

--- app/src/main/java/com/example/sanguage/DatabaseFragment.java
+++ app/src/main/java/com/example/sanguage/DatabaseFragment.java
@@ -2,13 +2,18 @@ package com.example.sanguage;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DatabaseFragment extends Fragment {
 
@@ -33,6 +38,35 @@ public class DatabaseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_database, container, false);
+        View view = inflater.inflate(R.layout.fragment_database, container, false);
+
+        TextView asdf = (TextView) view.findViewById(R.id.zastąpić_słówkami);
+
+        registerForContextMenu(asdf);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getActivity().getMenuInflater().inflate(R.menu.context_database, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_database_show_more:
+                Toast.makeText(getContext(), "show more", Toast.LENGTH_SHORT).show();
+                //show selected vocabulary on a flashcard
+                return true;
+            case R.id.context_database_delete:
+                Toast.makeText(getContext(), "delete", Toast.LENGTH_SHORT).show();
+                //delete selected vocabulary from database
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/sanguage/SearchFragment.java
+++ app/src/main/java/com/example/sanguage/SearchFragment.java
@@ -2,46 +2,29 @@ package com.example.sanguage;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ListView database_lv;
 
     public SearchFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
+    public static DatabaseFragment newInstance(String param1, String param2) {
+        DatabaseFragment fragment = new DatabaseFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +32,41 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        TextView asdf = (TextView) view.findViewById(R.id.search_vocabulary_tv);
+
+        registerForContextMenu(asdf);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getActivity().getMenuInflater().inflate(R.menu.context_search, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_search_show_more:
+                Toast.makeText(getContext(), "showing more", Toast.LENGTH_SHORT).show();
+                //show selected vocabulary on a flashcard
+                return true;
+            case R.id.context_search_add:
+                Toast.makeText(getContext(), "add", Toast.LENGTH_SHORT).show();
+                //delete selected vocabulary from database
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }

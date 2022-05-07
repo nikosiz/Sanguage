55f0f4e97ef3146d8b63b8e9aaefff6924913901
NB <nikodemb@student.agh.edu.pl>
2022-05-07 18:20:17 +0200

    Added transition animations

--- app/src/main/java/com/example/sanguage/ChooseActivity.java
+++ app/src/main/java/com/example/sanguage/ChooseActivity.java
@@ -1,20 +1,26 @@
 package com.example.sanguage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.transition.Fade;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ChooseActivity extends AppCompatActivity {
 
     private Button choose_sign_up_btn, choose_sign_in_btn;
     private TextView choose_skip_btn;
+    private ImageView mascot;
+    private TextView logo;
 
     @Override
     public void onBackPressed() {
@@ -27,22 +33,35 @@ public class ChooseActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.choose_activity);
 
+        Fade fade = new Fade();
+        View decor  = getWindow().getDecorView();
+        fade.excludeTarget(decor.findViewById(R.id.choose), true);
+        getWindow().setEnterTransition(fade);
+        getWindow().setExitTransition(fade);
+
         choose_sign_up_btn = findViewById(R.id.choose_sign_up_btn);
         choose_sign_up_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
 
         choose_sign_in_btn = findViewById(R.id.choose_log_in_btn);
+        mascot = findViewById(R.id.choose_app_mascot);
+        logo = findViewById(R.id.choose_app_logo);
 
         choose_sign_in_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
-                startActivity(intent);
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(ChooseActivity.this, mascot, ViewCompat.getTransitionName(mascot));
+                ActivityOptionsCompat options_logo = ActivityOptionsCompat.makeSceneTransitionAnimation(ChooseActivity.this, logo, ViewCompat.getTransitionName(logo));
+                options_logo.toBundle();
+                startActivity(intent, options.toBundle());
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
 
@@ -53,7 +72,14 @@ public class ChooseActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), AppWindowNoAccount.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }

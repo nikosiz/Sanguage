4673830e90fb618bd6bfb504aeba997b66631b39
NB <nikodemb@student.agh.edu.pl>
2022-05-07 14:40:29 +0200

    Changed flashcard layout

---
+++ app/src/main/java/com/example/sanguage/AppWindowNoAccount.java
@@ -0,0 +1,14 @@
+package com.example.sanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppWindowNoAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_window_no_account);
+    }
+}
